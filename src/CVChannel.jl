d65281be3a6d41a10730f9eab1aaf48de6cb636a
Ian George <igeorge3@illinois.edu>
2021-03-22 14:35:05 -0500

    added functions minEnt and minEntPPT

--- src/CVChannel.jl
+++ src/CVChannel.jl
@@ -1,9 +1,10 @@
 module CVChannel
 
-using Convex: partialtranspose, eigmin
+using Convex
+using MosekTools
+using LinearAlgebra
 
-export isPPT
-
+export isPPT, minEnt, minEntPPT
 """
     isPPT(x, sys :: Int, dims :: Vector) :: Bool
 
@@ -23,6 +24,64 @@ function isPPT(x,sys::Int,dims::Vector) :: Bool
         return true
     else
         return false
+    end
+end
+"""
+    minEnt(x,dimA :: Int ,dimB :: Int, dual=true :: Boolean)
+
+This function returns both the objective value of the
+SDP for the min-entropy along with its optimizer. To determine
+the min-entropy, take -log2 of the objective value.
+(See https://arxiv.org/abs/1504.00233 for further details about
+the min-entropy). It can run the dual or the primal (Note: we label
+the primal as the maximization problem unlike in the above reference.)
+"""
+function minEnt(ρ, dimA :: Int, dimB :: Int, dual=true :: Bool)
+    if dual == true
+        identMat = Matrix{Float64}(I, dimA, dimA)
+        Y = HermitianSemidefinite(dimB)
+        objective = real(tr(Y))
+        constraint = [kron(identMat , Y) ⪰ ρ]
+        problem = minimize(objective,constraint)
+        solve!(problem, () -> Mosek.Optimizer(QUIET = true))
+        return problem.optval, Y.value
+    else
+        X = HermitianSemidefinite(dimA*dimB)
+        objective = real(tr(ρ' * X))
+        constraint = partialtrace(X, 1, [dimA,dimB]) == Matrix{Float64}(I,dimB,dimB)
+        problem = maximize(objective,constraint)
+        solve!(problem, () -> Mosek.Optimizer(QUIET = true))
+        return problem.optval, X.value
+    end
+end
+"""
+    minEnt(x,dimA :: Int ,dimB :: Int, dual=true :: Boolean)
+
+This function returns both the objective value of the
+SDP for the min-entropy along with its optimizer when the optimizer is
+restricted to the PPT cone. This has various interpretations. It can run
+the dual or the primal. Notation follows Dropbox Note. (Note: we label
+the primal as the maximization problem following Watrous.)
+"""
+function minEntPPT(ρ, dimA :: Int, dimB :: Int, dual=true :: Bool)
+    if dual == true
+        identMat = Matrix{Float64}(I, dimA, dimA)
+        Y1 = ComplexVariable(dimB,dimB)
+        Y2 = HermitianSemidefinite(dimA*dimB)
+        objective = real(tr(Y1))
+        constraints = [kron(identMat,Y1) - partialtranspose(Y2, 2 , [dimA,dimB]) ⪰ ρ,
+                                Y1' - Y1 == zeros(dimB,dimB)] #Forces Hermiticity
+        problem = minimize(objective,constraints)
+        solve!(problem, () -> Mosek.Optimizer(QUIET = true))
+        return problem.optval, Y1.value, Y2.value
+    else
+        X = HermitianSemidefinite(dimA*dimB)
+        objective = real(tr(ρ' * X))
+        constraints = [partialtrace(X, 1, [dimA,dimB]) == Matrix{Float64}(I,dimB,dimB),
+                       partialtranspose(X,2,[dimA,dimB]) ⪰ 0]
+        problem = maximize(objective,constraints)
+        solve!(problem, () -> Mosek.Optimizer(QUIET = true))
+        return problem.optval, X.value
     end
 end
 
--- test/CVChannel.jl
+++ test/CVChannel.jl
@@ -2,6 +2,13 @@ using Test
 using CVChannel
 
 @testset "./src/CVChannel.jl" begin
-    @test isPPT([1 0 0 0 ; 0  1 0 0 ; 0 0 1 0 ; 0 0 0 1],2,[2,2])
-    @test !isPPT([1 0 0 1 ; 0 0 0 0; 0 0 0 0 ; 1 0 0 1],2,[2,2])
+    maxEntState = 0.5 * [1 0 0 1 ; 0 0 0 0 ; 0 0 0 0 ; 1 0 0 1]
+    maxMixState = 0.25 * [1 0 0 0 ; 0 1 0 0 ; 0 0 1 0 ; 0 0 0 1]
+    @test isPPT(maxMixState,2,[2,2])
+    @test !isPPT(maxEntState,2,[2,2])
+    @test (abs(minEnt(maxMixState,2,2)[1]-1/2) <= 1e-6) == true
+    @test (abs(minEnt(maxMixState,2,2,false)[1]-1/2) <= 1e-6) == true
+    @test (abs(minEnt(maxEntState,2,2)[1]- 2) <= 1e-6) == true
+    @test (abs(minEntPPT(maxMixState,2,2,false)[1]-1/2) <= 1e-6) == true
+    @test (abs(minEntPPT(maxEntState,2,2,false)[1]-1) <= 1e-6) == true
 end
