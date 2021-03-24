83628e5bc3019fcde5fc2548d21f0072c7af2685
Brian Doolittle <brian.d.doolittle@gmail.com>
2021-03-24 12:34:27 -0500

    initial fix for mosek licensing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,12 @@
 using Test, SafeTestsets
 
+include("../mosek_license.jl")
+
+@testset "checking for MOSEK license" begin
+    @test haskey(ENV,"MOSEKLM_LICENSE_FILE")
+    @test isfile(ENV["MOSEKLM_LICENSE_FILE"])
+end
+
 println("importing CVChannel")
 @time using CVChannel
 
