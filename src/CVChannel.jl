b7dcce52b4a1f78aeeafdb93976260f4a4f38019
igeorge3 <80070088+igeorge3@users.noreply.github.com>
2021-03-25 14:46:45 -0500

    Update CVChannel.jl

--- src/CVChannel.jl
+++ src/CVChannel.jl
@@ -27,62 +27,93 @@ function isPPT(x,sys::Int,dims::Vector) :: Bool
     end
 end
 """
-    minEnt(x,dimA :: Int ,dimB :: Int, dual=true :: Boolean)
+    minEntropyPrimal(\\rho,dimA :: Int ,dimB :: Int) :: Tuple{Float64,  Matrix{ComplexF64}}
 
-This function returns both the objective value of the
-SDP for the min-entropy along with its optimizer. To determine
-the min-entropy, take -log2 of the objective value.
-(See https://arxiv.org/abs/1504.00233 for further details about
-the min-entropy). It can run the dual or the primal (Note: we label
-the primal as the maximization problem unlike in the above reference.)
+This function solves the SDP
+```math
+ \min \{ \langle \rho, X \rangle \text{ subject to }  \text{Tr}_{A}(X) = I_{B} , X \succeq 0 \}
+```
+and returns the optimal value and the optimizer, X. This is the SDP corresponding to the min-entropy.
+To determine the min-entropy, take ``-\\log_{2}`` of the objective value.
+(See [Section 6.1 of this reference](https://arxiv.org/abs/1504.00233 
+"Quantum Information Processing with Finite Resources) for further details about
+the min-entropy). Note: we label the primal as the maximization problem unlike
+in the above reference.
 """
-function minEnt(ρ, dimA :: Int, dimB :: Int, dual=true :: Bool)
-    if dual == true
-        identMat = Matrix{Float64}(I, dimA, dimA)
-        Y = HermitianSemidefinite(dimB)
-        objective = real(tr(Y))
-        constraint = [kron(identMat , Y) ⪰ ρ]
-        problem = minimize(objective,constraint)
-        solve!(problem, () -> Mosek.Optimizer(QUIET = true))
-        return problem.optval, Y.value
-    else
-        X = HermitianSemidefinite(dimA*dimB)
-        objective = real(tr(ρ' * X))
-        constraint = partialtrace(X, 1, [dimA,dimB]) == Matrix{Float64}(I,dimB,dimB)
-        problem = maximize(objective,constraint)
-        solve!(problem, () -> Mosek.Optimizer(QUIET = true))
-        return problem.optval, X.value
-    end
+function minEntropyPrimal(ρ, dimA :: Int, dimB :: Int) :: Tuple{Float64,  Matrix{ComplexF64}}
+    X = HermitianSemidefinite(dimA*dimB)
+    objective = real(tr(ρ' * X))
+    constraint = partialtrace(X, 1, [dimA,dimB]) == Matrix{Float64}(I,dimB,dimB)
+    problem = maximize(objective,constraint)
+    solve!(problem, () -> Mosek.Optimizer(QUIET = true))
+    return problem.optval, X.value
 end
 """
-    minEnt(x,dimA :: Int ,dimB :: Int, dual=true :: Boolean)
+    minEntropyDual(\\rho,dimA :: Int ,dimB :: Int) :: Tuple{Float64,  Matrix{ComplexF64}}
 
-This function returns both the objective value of the
-SDP for the min-entropy along with its optimizer when the optimizer is
-restricted to the PPT cone. This has various interpretations. It can run
-the dual or the primal. Notation follows Dropbox Note. (Note: we label
-the primal as the maximization problem following Watrous.)
+This function solves the SDP
+```math
+ \min \{ \langle Y \rangle \text{ subject to }  I_{A} \otimes Y \succeq \rho, Y \in \text{Herm}(B) \}
+```
+and returns the optimal value and the optimizer, Y. This is the dual problem for the SDP for the min-entropy. To determine
+the min-entropy, take `-log2()' of the objective value.
+(See [Section 6.1 of this reference](https://arxiv.org/abs/1504.00233 
+"Quantum Information Processing with Finite Resources) for further details about
+the min-entropy). Note: we label the primal as the maximization problem unlike
+in the above reference.
 """
-function minEntPPT(ρ, dimA :: Int, dimB :: Int, dual=true :: Bool)
-    if dual == true
-        identMat = Matrix{Float64}(I, dimA, dimA)
-        Y1 = ComplexVariable(dimB,dimB)
-        Y2 = HermitianSemidefinite(dimA*dimB)
-        objective = real(tr(Y1))
-        constraints = [kron(identMat,Y1) - partialtranspose(Y2, 2 , [dimA,dimB]) ⪰ ρ,
-                                Y1' - Y1 == zeros(dimB,dimB)] #Forces Hermiticity
-        problem = minimize(objective,constraints)
-        solve!(problem, () -> Mosek.Optimizer(QUIET = true))
-        return problem.optval, Y1.value, Y2.value
-    else
-        X = HermitianSemidefinite(dimA*dimB)
-        objective = real(tr(ρ' * X))
-        constraints = [partialtrace(X, 1, [dimA,dimB]) == Matrix{Float64}(I,dimB,dimB),
-                       partialtranspose(X,2,[dimA,dimB]) ⪰ 0]
-        problem = maximize(objective,constraints)
-        solve!(problem, () -> Mosek.Optimizer(QUIET = true))
-        return problem.optval, X.value
-    end
+function minEntropyDual(ρ, dimA :: Int, dimB :: Int) :: Tuple{Float64,  Matrix{ComplexF64}}
+    identMat = Matrix{Float64}(I, dimA, dimA)
+    Y = HermitianSemidefinite(dimB)
+    objective = real(tr(Y))
+    constraint = [kron(identMat , Y) ⪰ ρ]
+    problem = minimize(objective,constraint)
+    solve!(problem, () -> Mosek.Optimizer(QUIET = true))
+    return problem.optval, Y.value
+end
+"""
+    minEntropyPPTPrimal(\\rho,dimA :: Int ,dimB :: Int) :: Tuple{Float64,  Matrix{ComplexF64}}
+
+This function solves the SDP
+```math
+ \min \{ \langle \rho, X \rangle \text{ subject to }  \text{Tr}_{A}(X) = I_{B} , \Gamma(X) \succeq 0, X \succeq 0 \}
+```
+where `` \\Gamma( \\cdot)'' is the partial transpose with respect to the second system, 
+and returns the optimal value and the optimizer, X.
+This is the dual problem for the SDP for the min-entropy restricted to the PPT cone. 
+This has various interpretations. Note: we label the primal as the maximization problem.
+"""
+function minEntropyPPTPrimal(ρ, dimA :: Int, dimB :: Int) :: Tuple{Float64,  Matrix{ComplexF64}}
+    X = HermitianSemidefinite(dimA*dimB)
+    objective = real(tr(ρ' * X))
+    constraints = [partialtrace(X, 1, [dimA,dimB]) == Matrix{Float64}(I,dimB,dimB),
+                   partialtranspose(X,2,[dimA,dimB]) ⪰ 0]
+    problem = maximize(objective,constraints)
+    solve!(problem, () -> Mosek.Optimizer(QUIET = true))
+    return problem.optval, X.value
+end
+"""
+    minEntropyPPTDual(\\rho,dimA :: Int ,dimB :: Int) :: Tuple{Float64,  Matrix{ComplexF64}, Matrix{ComplexF64}}
+
+This function solves the SDP
+```math
+ \min \{ \langle Y_{1} \rangle \text{ subject to }  I_{A} \otimes Y_{1} - \Gamma(Y_{2}) \succeq \rho, Y_{2} \succeq 0, Y_{1} \in \text{Herm}(B) \}
+```
+where `` \\Gamma( \\cdot)`` is the partial transpose with respect to the second system, 
+and returns the optimal value and optimizer, ``(Y_1 , Y_2 )``.
+This is the dual problem for the SDP for the min-entropy restricted to the PPT cone. 
+This has various interpretations. Note: we label the primal as the maximization problem.
+"""
+function minEntropyPPTDual(ρ, dimA :: Int, dimB :: Int, dual=true :: Bool) :: Tuple{Float64,  Matrix{ComplexF64}, Matrix{ComplexF64}}
+    identMat = Matrix{Float64}(I, dimA, dimA)
+    Y1 = ComplexVariable(dimB,dimB)
+    Y2 = HermitianSemidefinite(dimA*dimB)
+    objective = real(tr(Y1))
+    constraints = [kron(identMat,Y1) - partialtranspose(Y2, 2 , [dimA,dimB]) ⪰ ρ,
+                   Y1' - Y1 == zeros(dimB,dimB)] #Forces Hermiticity
+    problem = minimize(objective,constraints)
+    solve!(problem, () -> Mosek.Optimizer(QUIET = true))
+    return problem.optval, Y1.value, Y2.value
 end
 
 end
